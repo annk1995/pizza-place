85b136247292c95b0b3dcd2fe0d901e99c00ed91
annk1995 <kabuikariuki052@gmail.com>
2022-03-20 22:24:49 +0300

    added to functionality

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 $('#order').submit(function(){
     var size = document.getElementById('size').value;
+    var type =document.getElementById('type').value
     var topping = document.getElementById('toppings').value;
     var cheese = document.getElementById('cheesee').value;
     var t = (parseInt(size)+parseInt(topping)+parseInt(cheese))
     document.getElementById('totalss').innerText = t;
+     
     $('#testss').show();
     return false;
 })
