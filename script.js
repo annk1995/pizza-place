85914e5bc1a2e30ce384463dad3f8ff3ee648a98
annk1995 <kabuikariuki052@gmail.com>
2022-03-20 23:50:41 +0300

    added functionality to the tabble i added

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 $('#order').submit(function(){
-    var size = document.getElementById('size').value;
+    var size = document.getElementById('sizes').value;
     var type =document.getElementById('type').value
     var topping = document.getElementById('toppings').value;
     var cheese = document.getElementById('cheesee').value;
     var t = (parseInt(size)+parseInt(topping)+parseInt(cheese))
     document.getElementById('totalss').innerText = t;
-     
+    document.getElementById('size').innerText = size;
+    document.getElementById('types').innerText =type
     $('#testss').show();
     return false;
 })
