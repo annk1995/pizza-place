d5687326d59483c4092d0addfcc00f98a3105e04
annk1995 <kabuikariuki052@gmail.com>
2022-03-21 11:18:44 +0300

    added fuctionality to size of food

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 $('#order').submit(function(){
     var size = document.getElementById('sizes').value;
+    console.log(size)
     var type =document.getElementById('type').value
     var topping = document.getElementById('toppings').value;
     var cheese = document.getElementById('cheesee').value;
     var t = (parseInt(size)+parseInt(topping)+parseInt(cheese))
+   
+   
     document.getElementById('totalss').innerText = t;
-    document.getElementById('size').innerText = size;
-    document.getElementById('types').innerText =type
+   //document.getElementById('sizes:selected').innerText  
+if (size == "400"){
+
+    document.getElementById('size').innerText = "small"
+}else if (size == "600"){
+
+    document.getElementById('size').innerText = "medium"
+}else{
+    document.getElementById('size').innerText = "large"
+}
+    document.getElementById('types').innerText =type;
+    document.getElementById('toppingss').innerText =topping;
+    
+
     $('#testss').show();
     return false;
 })
