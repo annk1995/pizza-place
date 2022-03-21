43d3b49b7c566af8ae72e693b2c2b6764ad4ae5d
annk1995 <kabuikariuki052@gmail.com>
2022-03-21 13:29:48 +0300

    added functionality to contact form

--- script.js
+++ script.js
@@ -30,11 +30,24 @@ if (size == "400"){
    }
    document.getElementById('cheese').innerText="cheesee"
 
-    
-   $("#checkout").submit(function(event){
-       event.preventDefault();
-       console.log('hi')
-   // $("#choise").toggle();
-  });
+    $('.choise').show();
    
 })
+
+$(document).ready(function() {
+    $('#contactUs').submit(function(event) {
+        var myName = $('#name').val();
+        var myEmail = $('#email').val();
+        var comment = $('#textarea').val();
+  
+        if (myName === '' || myEmail === '' || comment === '') {
+            alert('Please fill in the required data');
+        } else {
+            alert(myName + ' ,we have received your message.Thank you for reaching out to us.');
+        }
+        event.preventDefault();
+        $('#name').val('');
+        $('#email').val('');
+        $('#textarea').val('');
+    });
+  });
